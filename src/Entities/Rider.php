843ad59c71d6c930f96aea0de112ff1132e97ae2
Bert Rowett <bertrowett@googlemail.com>
2023-09-11 12:31:25 +0200

    Rider entity created

--- src/Entities/Rider.php
+++ src/Entities/Rider.php
@@ -1,2 +1,45 @@
 <?php
 
+readonly class Rider
+{
+    public int $id;
+    public string $name;
+    public string $image;
+    public string $team;
+    public string $nation;
+    public string $dob;
+    public int|null $giroGC;
+    public int|null $tourGC;
+    public int|null $vueltaGC;
+    public int|null $giroStages;
+    public int|null $tourStages;
+    public int|null $vueltaStages;
+
+    public function __construct(
+        int $id,
+        string $name,
+        string $image,
+        string $team,
+        string $nation,
+        string $dob,
+        int|null $giroGC,
+        int|null $tourGC,
+        int|null $vueltaGC,
+        int|null $giroStages,
+        int|null $tourStages,
+        int|null $vueltaStages
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->image = $image;
+        $this->team = $team;
+        $this->nation = $nation;
+        $this->dob = $dob;
+        $this->giroGC = $giroGC;
+        $this->tourGC = $tourGC;
+        $this->vueltaGC = $vueltaGC;
+        $this->giroStages = $giroStages;
+        $this->tourStages = $tourStages;
+        $this->vueltaStages = $vueltaStages;
+    }
+}
