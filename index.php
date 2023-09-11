6b1dfda6c06f27a44a2549060738b8202de3cebb
Bert Rowett <bertrowett@googlemail.com>
2023-09-11 13:30:57 +0200

    HeadHtml class (echoes HTML head) created + instantiated in index file

--- index.php
+++ index.php
@@ -1,38 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Pro Cyclists Collection</title>
-
-    <meta name="description" content="Template HTML file">
-    <meta name="author" content="iO Academy">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-    <link rel="stylesheet" href="css/styles.css">
-
-    <link rel="icon" href="images/favicon.png" sizes="192x192">
-    <link rel="shortcut icon" href="images/favicon.png">
-    <link rel="apple-touch-icon" href="images/favicon.png">
-
-    <script defer src="js/index.js"></script>
-</head>
-
-<body>
+<!-- <body>
 
     <h1>Website Template</h1>
 
 </body>
 
-</html>
+</html> -->
 
 <?php
 
+use Collection\HTML\HeadHtml;
 use Collection\Models\RidersModel;
 
 require_once 'database.php';
 require_once 'vendor/autoload.php';
 
 $ridersModel = new RidersModel($db);
+$headHtml = new HeadHtml();
+
+
+$headHtml->display();
