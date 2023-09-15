d91cedb9f8cd9a9cc0a705ceb6c0b753e4780712
Bert Rowett <bertrowett@googlemail.com>
2023-09-15 13:50:03 +0200

    Created getRetiredRiders + unretireRider methods

--- src/Models/RidersModel.php
+++ src/Models/RidersModel.php
@@ -68,6 +68,60 @@ class RidersModel
         return $allRiders;
     }
 
+    public function getRetiredRiders(): array|false
+    {
+        $query = $this->db->prepare('
+            SELECT
+            `riders`.`id`,
+            `riders`.`name`,
+            `riders`.`image`,
+            `teams`.`team`,
+            `nations`.`nation`,
+            `riders`.`dob`,
+            `riders`.`giro_gc`,
+            `riders`.`tour_gc`,
+            `riders`.`vuelta_gc`,
+            `riders`.`giro_stages`,
+            `riders`.`tour_stages`,
+            `riders`.`vuelta_stages`,
+            `riders`.`retired`
+            FROM `riders`
+                INNER JOIN `teams`
+                    ON `riders`.`team_id` = `teams`.`id`
+                INNER JOIN `nations`
+                    ON `riders`.`nation_id` = `nations`.`id`
+            WHERE `riders`.`retired` = 1
+            ORDER BY `riders`.`id` DESC;
+        ');
+        $query->execute();
+        $data = $query->fetchAll();
+
+        if (!$data) {
+            return false;
+        }
+
+        $allRiders = [];
+        foreach ($data as $datum) {
+            $rider = new Rider(
+                $datum['id'],
+                $datum['name'],
+                $datum['image'],
+                $datum['team'],
+                $datum['nation'],
+                $datum['dob'],
+                $datum['giro_gc'],
+                $datum['tour_gc'],
+                $datum['vuelta_gc'],
+                $datum['giro_stages'],
+                $datum['tour_stages'],
+                $datum['vuelta_stages'],
+                $datum['retired']
+            );
+            $allRiders[] = $rider;
+        }
+        return $allRiders;
+    }
+
     public function getActiveRidersByTeamId($teamId): array|false
     {
         $query = $this->db->prepare("
@@ -282,6 +336,12 @@ class RidersModel
     public function retireRider(int $id): bool
     {
         $query = $this->db->prepare("UPDATE `riders` SET `retired` = 1 WHERE `id` = $id;");
+        return $query->execute();
+    }
+
+    public function unretireRider(int $id): bool
+    {
+        $query = $this->db->prepare("UPDATE `riders` SET `retired` = 0 WHERE `id` = $id;");
         return $query->execute();
     }
 }
