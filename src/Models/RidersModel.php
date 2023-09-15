eb0af2fa884761f820a1d4ff224c78556b664fbb
Bert Rowett <bertrowett@googlemail.com>
2023-09-15 11:56:58 +0200

    Created Team entity. Created getAllTeams + getActiveRidersByTeamId methods

--- src/Models/RidersModel.php
+++ src/Models/RidersModel.php
@@ -65,7 +65,60 @@ class RidersModel
             );
             $allRiders[] = $rider;
         }
+        return $allRiders;
+    }
 
+    public function getActiveRidersByTeamId($teamId): array|false
+    {
+        $query = $this->db->prepare("
+            SELECT
+            `riders`.`id`,
+            `riders`.`name`,
+            `riders`.`image`,
+            `teams`.`team`,
+            `nations`.`nation`,
+            `riders`.`dob`,
+            `riders`.`giro_gc`,
+            `riders`.`tour_gc`,
+            `riders`.`vuelta_gc`,
+            `riders`.`giro_stages`,
+            `riders`.`tour_stages`,
+            `riders`.`vuelta_stages`,
+            `riders`.`retired`
+            FROM `riders`
+                INNER JOIN `teams`
+                    ON `riders`.`team_id` = `teams`.`id`
+                INNER JOIN `nations`
+                    ON `riders`.`nation_id` = `nations`.`id`
+            WHERE `riders`.`retired` = 0 AND `teams`.`id` = $teamId
+            ORDER BY `riders`.`id` DESC;
+        ");
+        $query->execute();
+        $data = $query->fetchAll();
+
+        if (!$data) {
+            return false;
+        }
+
+        $allRiders = [];
+        foreach ($data as $datum) {
+            $rider = new Rider(
+                $datum['id'],
+                $datum['name'],
+                $datum['image'],
+                $datum['team'],
+                $datum['nation'],
+                $datum['dob'],
+                $datum['giro_gc'],
+                $datum['tour_gc'],
+                $datum['vuelta_gc'],
+                $datum['giro_stages'],
+                $datum['tour_stages'],
+                $datum['vuelta_stages'],
+                $datum['retired']
+            );
+            $allRiders[] = $rider;
+        }
         return $allRiders;
     }
 
