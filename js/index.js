e5b8b72c142e33f588b05ca07596f82609f7edd1
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2024-10-30 09:52:23 +0100

    Add FE validation for max length of name, image, team and nation to prevent this not being caught until BE validation

--- js/index.js
+++ js/index.js
@@ -30,19 +30,17 @@ function handleSubmit(submit) {
     const vueltaStagesInput = document.querySelector('#vueltaStages').value;
 
     const invalidInputs = [];
+
+    const trimmedNameInput = nameInput.trim();
+    const trimmedImageInput = imageInput.trim();
+    const trimmedTeamInput = teamInput.trim();
+    const trimmedNationInput = nationInput.trim();
     const dobRegex = /^(\d{4})-(\d{2})-(\d{2})$/;
 
-    function isValidDate(dateString) {
-        const [year, month, day] = dateString.split('-').map(Number);
-        const date = new Date(year, month - 1, day); // Month is zero-indexed in JS Date
-
-        return date && date.getFullYear() === year && date.getMonth() + 1 === month && date.getDate() === day;
-    }
-
-    if (nameInput.trim() === '') invalidInputs.push('name');
-    if (imageInput.trim() === '') invalidInputs.push('image');
-    if (teamInput.trim() === '') invalidInputs.push('team');
-    if (nationInput.trim() === '') invalidInputs.push('nation');
+    if (trimmedNameInput === '' || trimmedNameInput.length > 999) invalidInputs.push('name');
+    if (trimmedImageInput === '' || trimmedImageInput.length > 999) invalidInputs.push('image');
+    if (trimmedTeamInput === '' || trimmedTeamInput.length > 999) invalidInputs.push('team');
+    if (trimmedNationInput === '' || trimmedNationInput.length > 999) invalidInputs.push('nation');
     if (!dobRegex.test(dobInput) || !isValidDate(dobInput)) invalidInputs.push('dob');
     if (!Number.isInteger(Number(giroGcInput)) || Number(giroGcInput) < 0) invalidInputs.push('giroGc');
     if (!Number.isInteger(Number(tourGcInput)) || Number(tourGcInput) < 0) invalidInputs.push('tourGc');
@@ -59,4 +57,11 @@ function handleSubmit(submit) {
             label.style.color = invalidInputs.includes(input) ? 'red' : 'black';
         });
     }
+}
+
+function isValidDate(dateString) {
+    const [year, month, day] = dateString.split('-').map(Number);
+    const date = new Date(year, month - 1, day); // Month is zero-indexed in JS Date
+
+    return date && date.getFullYear() === year && date.getMonth() + 1 === month && date.getDate() === day;
 }
