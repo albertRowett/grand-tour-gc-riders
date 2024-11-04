albertRowett/grand-tour-gc-riders

commit feb432649617d2086d7163c8dc9409706855fb5a

Refactor index.php to contain all code required for DB interactions and indexHtml.php to just contain code required to display correct HTML for the page

index.php
@@ -15,22 +15,23 @@ $headHtml = new HeadHtml();
 $headerHtml = new HeaderHtml();
 $indexHtml = new IndexHtml();
 
-// Handling form submission (rider retirement)
-$allRiders = $ridersModel->getRiders(0, null);
+$riders = $ridersModel->getRiders(0, null);
+$teams = $teamsModel->getTeams();
 
-foreach ($allRiders as $rider) {
-    $buttonClicked = $_POST[$rider->id] ?? false;
+// Handle form submission (edit/retire rider)
+if ($riders) {
+    foreach ($riders as $rider) {
+        $buttonClicked = $_POST[$rider->id] ?? false;
 
-    if ($buttonClicked === 'Retire') {
-        if (!$ridersModel->toggleRiderRetirement($rider->id, 1)) {
-            header('Location: index.php?error=1');
+        if ($buttonClicked === 'Retire') {
+            $ridersModel->toggleRiderRetirement($rider->id, 1) ? header('Location: index.php') : header('Location: index.php?error=1');
+        } elseif ($buttonClicked === 'Edit') {
+            header("Location: editRider.php?id=$rider->id");
         }
-    } elseif ($buttonClicked === 'Edit') {
-        header("Location: editRider.php?id=$rider->id");
     }
 }
 
-// Displaying the page
+// Display page
 $headHtml->display();
 $headerHtml->display();
-$indexHtml->display($ridersModel, $teamsModel);
+$indexHtml->display($riders, $teams);

src/HTML/PageContents/IndexHtml.php
@@ -2,8 +2,6 @@
 
 namespace Collection\HTML\PageContents;
 
-use Collection\Models\RidersModel;
-use Collection\Models\TeamsModel;
 use DateTime;
 
 class IndexHtml
@@ -23,7 +21,7 @@ class IndexHtml
         }
     }
 
-    public function display(RidersModel $ridersModel, TeamsModel $teamsModel): void
+    public function display(array|false $riders, array|false $teams): void
     {
         echo "
             <form class='filters'>
@@ -31,10 +29,8 @@ class IndexHtml
                 <select name='team' id='teams' onchange='this.form.submit()'>
                 <option value='0'>Select team</option>";
 
-        $allTeams = $teamsModel->getAllTeams();
-
-        if ($allTeams) {
-            foreach ($allTeams as $team) {
+        if ($teams) {
+            foreach ($teams as $team) {
                 echo "<option value='$team->id'>$team->team</option>";
             }
         }
@@ -46,9 +42,6 @@ class IndexHtml
             <div class='ridersContainer'>
                 <p class='dbError'>{$this->retireRiderError()}</p>
         ";
-
-        $teamId = $_GET['team'] ?? null;
-        $riders = $ridersModel->getRiders(0, $teamId);
 
         if ($riders) {
             $today = new DateTime(date('y-m-d'));

src/Models/TeamsModel.php
@@ -14,7 +14,7 @@ class TeamsModel
         $this->db = $db;
     }
 
-    public function getAllTeams(): array|false
+    public function getTeams(): array|false
     {
         $query = $this->db->prepare('SELECT `id`, `team` FROM `teams` ORDER BY `team` ASC;');
         $query->execute();

commit 90408fc3a79e28b6d5a4dc44cfccff9cbf3efda9

Fix: restore ability to filter riders by team, which was inadvertently broken in feb432649617d2086d7163c8dc9409706855fb5a

index.php
@@ -15,7 +15,13 @@ $headHtml = new HeadHtml();
 $headerHtml = new HeaderHtml();
 $indexHtml = new IndexHtml();
 
-$riders = $ridersModel->getRiders(0, null);
+$teamId = $_GET['team'] ?? null;
+if (intval($teamId) != $teamId) { // i.e. $teamId is not int (or null)
+    header('Location: index.php');
+    exit;
+}
+
+$riders = $ridersModel->getRiders(0, $teamId);
 $teams = $teamsModel->getTeams();
 
 // Handle form submission (edit/retire rider)