f0474cf724209cc22531373207558b754b19ac65
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2024-11-05 08:05:41 +0100

    Complete HTML head and style this code for maximum clarity

--- src/HTML/HeadHtml.php
+++ src/HTML/HeadHtml.php
@@ -6,24 +6,27 @@ class HeadHtml
 {
     public function display(): void
     {
-        echo
-        '<head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
+        echo '
+        <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
 
-            <title>Grand Tour GC Riders</title>
+                <title>Grand Tour GC Riders</title>
 
-            <meta name="description" content="Template HTML file">
-            <meta name="author" content="iO Academy">
+                <meta name="description" content="Collection of the top grand tour GC riders">
+                <meta name="author" content="Albert Rowett">
 
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-            <link rel="stylesheet" href="css/styles.css">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+                <link rel="stylesheet" href="css/styles.css">
 
-            <link rel="icon" href="images/favicon.png" sizes="192x192">
-            <link rel="shortcut icon" href="images/favicon.png">
-            <link rel="apple-touch-icon" href="images/favicon.png">
+                <link rel="icon" href="images/favicon.png" sizes="192x192">
+                <link rel="shortcut icon" href="images/favicon.png">
+                <link rel="apple-touch-icon" href="images/favicon.png">
 
-            <script defer src="js/index.js"></script>
-        </head>';
+                <script defer src="js/index.js"></script>
+            </head>
+        ';
     }
 }
