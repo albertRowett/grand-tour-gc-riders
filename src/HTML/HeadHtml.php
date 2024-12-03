a39b73e00afa49b65dd7352ede5c45a0a3bf1486
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2024-12-03 11:11:55 +0100

    Add Open Graph meta tags for improved link sharing metadata

--- src/HTML/HeadHtml.php
+++ src/HTML/HeadHtml.php
@@ -17,6 +17,14 @@ class HeadHtml
 
                 <meta name="description" content="Collection of the top grand tour GC riders">
                 <meta name="author" content="Albert Rowett">
+                <meta property="og:title" content="Grand Tour GC Riders" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/ab138268-af26-4938-9cd6-e046af4660f8.png?token=fwWCvfFeW6vDsB76Lg8GZW5mN_C9r0J7Uhp5IeFwvDw&height=627&width=1200&expires=33269219737" />
+                <meta property="og:image:alt" content="Collection of the top grand tour GC riders" />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="627" />
+                <meta property="og:description" content="Collection of the top grand tour GC riders" />
+                <meta property="og:url" content="https://grand-tour-gc-riders.2023-bertr.dev.io-academy.uk/" />
 
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
                 <link rel="stylesheet" href="css/styles.css">
