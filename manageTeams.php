0702b1ec3a659d4425e380042cc48b7e4466b047
Albert Rowett <139590109+albertRowett@users.noreply.github.com>
2025-01-13 12:10:05 +0100

    Create styled manageTeams page containing form for marking teams active/inactive

--- manageTeams.php
+++ manageTeams.php
@@ -4,16 +4,21 @@ use Collection\HTML\HeadHtml;
 use Collection\HTML\PageContents\FooterHtml;
 use Collection\HTML\PageContents\HeaderHtml;
 use Collection\HTML\PageContents\ManageTeamsHTML;
+use Collection\Models\TeamsModel;
 
+require_once 'database.php';
 require_once 'vendor/autoload.php';
 
+$teamsModel = new TeamsModel($db);
 $headHtml = new HeadHtml();
 $headerHtml = new HeaderHtml();
 $manageTeamsHtml = new ManageTeamsHTML();
 $footerHtml = new FooterHtml();
 
+$teams = $teamsModel->getTeams();
+
 // Display page
 $headHtml->display();
 $headerHtml->display();
-$manageTeamsHtml->display();
+$manageTeamsHtml->display($teams);
 $footerHtml->display();
--- src/HTML/PageContents/ManageTeamsHtml.php
+++ src/HTML/PageContents/ManageTeamsHtml.php
@@ -4,7 +4,45 @@ namespace Collection\HTML\PageContents;
 
 class ManageTeamsHTML
 {
-    public function display(): void {
-        echo "<main></main>";
+    private function manageTeamsError() {
+        if (isset($_GET['error']) && $_GET['error'] === '1') {
+            return 'An error occurred while updating the teams list. Please try again later.';
+        }
+    }
+
+    public function display(array|false $teams): void {
+        echo "
+        <main>
+            <p class='errorMsg'>{$this->manageTeamsError()}</p>
+            <div class='teamsFormContainer'>
+                <h2>Select Active Teams:</h2>
+        ";
+
+        if (!$teams) {
+            echo "<p>No teams found</p>";
+        } else {
+            echo "<form class='teamsForm' method='POST'>";
+
+            foreach ($teams as $team) {
+                echo "
+                <div>
+                    <input type='checkbox' id='$team->team' name='$team->id' " . ($team->active ? ' checked' : '') . " />
+                    <label for='$team->team' class='teamCheckbox'>$team->team</label>
+                </div>
+            ";
+            }
+
+        echo "
+            <div class='teamsFormSubmit'>
+                <input type='submit' name='submit' value='Submit' />
+            </div>
+        </form>
+        ";
+        }
+
+        echo "
+            </div>
+        </main>
+        ";
     }
 }
